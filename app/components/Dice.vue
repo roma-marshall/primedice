0599bbf5746c5792d292b0c8888a9872e738958e
roma_marshall <roma_marshall@outlook.com>
2025-09-14 22:54:42 +0200

    add dice component

--- app/components/Dice.vue
+++ app/components/Dice.vue
@@ -1,10 +1,29 @@
 <template>
   <div>
-    <span></span>
-    <button>roll dice</button>
+    <div class="flex flex-col py-20">
+        <div class="flex justify-around my-auto">
+            <div class="flex justify-center items-center text-lg border size-24">
+                {{ diceLeft }}
+                </div>
+            <div class="flex justify-center items-center text-lg border size-24">
+                {{ diceRight }}
+            </div>
+        </div>
+        <div class="flex justify-center">
+            <button @click=roll class="border rounded-lg py-1 px-5 bg-gray-200 cursor-pointer active:bg-fuchsia-200 active:scale-95 transition transform dutaion-150">roll dice</button>
+        </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 
+const diceLeft = ref(1)
+const diceRight = ref(1)
+
+const roll = () => {
+    diceLeft.value = Math.floor(Math.random() * 6) + 1
+    diceRight.value = Math.floor(Math.random() * 6) + 1
+}
 </script>
