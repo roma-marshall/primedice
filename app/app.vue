7212a127995246b09f4178433c13447805faa9a8
roma_marshall <roma_marshall@outlook.com>
2025-09-14 22:25:24 +0200

    Init commit

---
+++ app/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <Dice />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ app/components/Dice.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <span></span>
+    <button>roll dice</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
