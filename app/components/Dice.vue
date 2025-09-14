427089e97e10f9d0893987c28b48af570414ec29
roma_marshall <roma_marshall@outlook.com>
2025-09-15 00:21:31 +0200

    add min bet

--- app/components/Dice.vue
+++ app/components/Dice.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="flex justify-around my-auto">
+    <div class="flex justify-around">
         <div class="flex justify-center items-center text-lg border size-24">
             {{ diceLeft }}
             </div>
@@ -8,8 +8,16 @@
             {{ diceRight }}
         </div>
     </div>
-    <div class="flex justify-center">
-        <button @click=roll class="px-4 py-2 bg-fuchsia-500 text-white rounded-lg hover:bg-fuchsia-600 active:scale-95 transition">Roll Dice</button>
+    <div class="flex flex-col items-center space-y-4">
+        <span>Place your bet (min bet 1 $FLOW)</span>
+        <div class="flex space-x-4">
+            <input type="number" class="flex w-20 p-2 border rounded-lg" v-model.number=bet>
+            <button @click="roll" class="px-4 py-2 rounded-lg text-white"
+                    :class="bet < 1 ? 'bg-gray-400' : 'bg-blue-500 hover:bg-blue-600'"
+                    :disabled="bet < 1">
+                Roll Dice
+            </button>
+        </div>
     </div>
   </div>
 </template>
@@ -19,6 +27,7 @@ import { ref } from 'vue'
 
 const diceLeft = ref(1)
 const diceRight = ref(1)
+const bet = ref(1)
 
 const roll = () => {
     diceLeft.value = Math.floor(Math.random() * 6) + 1
